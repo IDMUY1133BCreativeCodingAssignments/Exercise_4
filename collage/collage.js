b2c9279f6b51f5e6d2240d94df3e1aa6a489386e
jht301 <jackhterzi@gmail.com>
2015-10-13 19:10:01 -0400

    update
    still not sure what's wrong. i tried looking up the problem but couldnt
    find anything

--- collage/collage.js
+++ collage/collage.js
@@ -23,12 +23,12 @@
  var wait; 
  var waterfall; 
  var why;
- 
+ var i = 0;
+var x = 0;
 var images = [king, prize, paint, wait, painting1, painting2, painting3, fav1, thumbs, fav2, angrysky, waterfall, nopeace, look, bush, motherbird, peacebird, question, SEC, why, because, beachpic, meditate, peace];
-/* var texts = [ "Once upon a time there was a king", "who offered a prize to the artist", "who would paint the most peaceful picture.", "Many artists tried.", "The king looked at all the pictures.", "But there were only two he really liked,", "and he had to choose between them.", "One picture was of a calm lake. The lake was a perfect mirror for peaceful towering mountains all around it. Overhead was a blue sky with fluffy white clouds.", "All who saw this picture thought that it was a perfect picture of peace.",
-"The other picture had mountains, too. But these were rugged and bare.", "Above was an angry sky, from which rain fell and in which lightning played.", "Down the side of the mountain tumbled a foaming waterfall.", "This did not look peaceful at all.", "But when the king looked closely, he saw behind the waterfall", "a tiny bush growing in a crack in the rock.", "In the bush a mother bird had built her nest.", "There, in the midst of the rush of angry water, sat the mother bird on her nest - in perfect peace.", "Which picture do you think won the prize?", "The king chose the second picture.", "Do you know why?", "Because, explained the king,", "peace does not mean to be in a place where there is no noise, trouble, or hard work.", "Peace means to be in the midst of all those things and still be calm in your heart.", "That is the real meaning of peace."];*/
+ var story = [ "Once upon a time there was a king", "who offered a prize to the artist", "who would paint the most peaceful picture.", "Many artists tried.", "The king looked at all the pictures.", "But there were only two he really liked,", "and he had to choose between them.", "One picture was of a calm lake. The lake was a perfect mirror for peaceful towering mountains all around it. Overhead was a blue sky with fluffy white clouds.", "All who saw this picture thought that it was a perfect picture of peace.",
+"The other picture had mountains, too. But these were rugged and bare.", "Above was an angry sky, from which rain fell and in which lightning played.", "Down the side of the mountain tumbled a foaming waterfall.", "This did not look peaceful at all.", "But when the king looked closely, he saw behind the waterfall", "a tiny bush growing in a crack in the rock.", "In the bush a mother bird had built her nest.", "There, in the midst of the rush of angry water, sat the mother bird on her nest - in perfect peace.", "Which picture do you think won the prize?", "The king chose the second picture.", "Do you know why?", "Because, explained the king,", "peace does not mean to be in a place where there is no noise, trouble, or hard work.", "Peace means to be in the midst of all those things and still be calm in your heart.", "That is the real meaning of peace."];
 
-images = [i];
 
                
 function preload(){
@@ -60,24 +60,28 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(1000,1000);
     
+    createCanvas(1000,1000);
 }
 
 
 
 function draw(){
-
- image(images[i], 400, 400);    
     
+ image(images[i], 400, 400); 
+ text(story[x], 500, 400);    
     
     
 }
 function mouseClicked(){
 
-   for(i = 0; i<images.length; i++){
-
+   for(i = 0; i<= images.length; i++){
+ 
    }
+    for(x = 0; x<= story.lengh; x++){
+        
+    }
+
 }
 
 
