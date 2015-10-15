67ae78c9d70500210a004a8b44bba0a7a0322160
JonWuIDM <jw4219@nyu.edu>
2015-10-15 10:13:17 -0400

    Notes
    Something New, can't get a zoom function to work.

--- JonatanWu_Exercise4/Exercise_4.js
+++ JonatanWu_Exercise4/Exercise_4.js
@@ -4,19 +4,20 @@
 
 var  image1;
 var  image2;
-var  text1;
-var  text2;
+var  zoom = 1;
+var  x = 1366;
+var  image3;
+var  y = 0;
+var  x1 =1500;
 
 
 function preload() {
     
     image1 = loadImage("data/back.jpg");
-    image2 = loadImage("data/Spaceback.jpg");
-    text1 = loadFont("data/space 1.TTF");
-    text2 = loadFont("data/BM space A9.ttf");    
+    image2 = loadImage("data/Spaceback.jpg"); // Default
+    image3 = loadImage("data/Spaceback2.jpg");
     
 }
-
 
 
 function setup() {
@@ -26,31 +27,68 @@ function setup() {
 
 function draw() {
     
-    //background with box
     background(image2);
-    rectMode(CENTER);
-    fill(50, 150, 200, 80);
+    words();
+    glass();
+    
+     if (keyIsPressed) {
+         if (keyIsPressed == 'a' || 'A'){
+        background(image3, 1366,667);
+     }
+     }
+    
+}
+
+    
+function glass() {
+    
+    fill(50, 150, 200, 60);
+    stroke();
+    strokeWeight(4);
+    ellipse(mouseX, mouseY, y, y);
+    y = y + 1;
+    if ( y <= 100) {
+        y = 50;
+        
+    }
+    
+    
+         }
+
+function words() {
+    
+      fill(214, 147, 45,100);
+    smooth();
+    textFont('Courier New');
     noStroke();
-    ellipse(mouseX, mouseY, 150, 150);
-    
-    
-    // if mousePressed Zoom in pic, if released zoom out
-    if (mousePressed) {
+    textSize(50);
+    text("by Jon.W", x1, 600);
+    x1 -= .5;
+    if ( x1 < -1000) {
+        x1 = 500;
     }
-    
-    // if keyPressed a new image appears, No release event
-    if (keyPressed) {
+     fill(250, 85, 90,100);
+    smooth();
+    textFont('Courier New');
+    noStroke();
+    textSize(75);
+    text("Space Telescope", x, 75);
+    x -= 1;
+    if ( x < -1000) {
+        x = 1000;
     }
+}
+
+
+
+
+
+
     
         
-    
-    fill(255);
-    textFont('text1');
-    textSize(100);
-    text("Space The Game", width/2-20, 50);
-    
+
 
     
     
-}
+
 
